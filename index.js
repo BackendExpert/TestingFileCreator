6c18bab36cdba1a517881a42e2eaf1c3380855fc
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-07-27 11:11:57 +0530

    Update index.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- index.js
+++ index.js
@@ -6,6 +6,18 @@ const fs = require('fs-extra');
 const path = require('path');
 const argv = require('yargs').argv;
 
+async function RunReactLoginSignInTW() {
+  try {
+      await fs.copy(sourceTW, destinationTW);
+      await fs.copy(sourceAppTW, destinationAppTW);
+      await fs.copy(sourcePR, destinationPR);
+      await fs.copy(sourceDash, destinationDash);
+      console.log('The Login and SignUp templete Successfully Createed');
+  } catch (err) {
+      console.error('Error copying folder:', err);
+  }
+}
+
 async function main() {
   try {
     // Prompt the user for a filename
